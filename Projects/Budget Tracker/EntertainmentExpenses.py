bd5e67ed0a88d34de213f412eb476774af0a7d76
Oregolele <oregolelemangwegape05@gmail.com>
2026-07-27 11:09:13 +0200

    Update EntertainmentExpenses.py
    populated first method in entertainment

--- Projects/Budget Tracker/EntertainmentExpenses.py
+++ Projects/Budget Tracker/EntertainmentExpenses.py
@@ -2,11 +2,46 @@ ordering_costs = []
 movie_tickets = []
 event_tickets = []
 recreational_activities = []
-media_sources = []
 vacation_funds = []
 
 def Entertain():
-    pass
+    total_expenses = 0
+    is_running = True
+    while is_running:
+        print("Welcome To ther Entertainment expenses calculator!")
+        option = input("Would you like to continue or quit?: ").lower()
+        if option == "continue":
+            try:
+                choice = int(input("What would youy like to calculate?\n" 
+                "1. Ordering and Dining costs.\n" 
+                "2. Movie expenses.\n" 
+                "3. Events/Concerts.\n" 
+                "4. Recreational activities or hobbies.\n" 
+                "5. Vacation or Trips"
+                "choose an option (1-5): "))
+                if choice == 1:
+                    total_expenses += OrderingAndDining()
+                elif choice ==2:
+                    total_expenses += MovieAndRental()
+                elif choice ==3:
+                    total_expenses += EventAndConcert()
+                elif choice ==4:
+                    total_expenses =+ RecreationalAndHobbies()
+                elif choice ==5:
+                    total_expenses += VacationAndTravel()
+                else:
+                    print("Invalid choice. Please choose between 1-5.")
+                    continue
+            except ValueError:
+                print("Please enter a numeric value.")
+                continue
+        elif option == "quit":
+            print("Goodbye!")
+            is_running = False
+        else:
+            print("Please enter either 'continue' or 'quit'.")
+            continue
+    return total_expenses
 
 def OrderingAndDining():
     pass
@@ -23,9 +58,6 @@ def EventAndConcert():
 def RecreationalAndHobbies():
     pass
 
-
-def MediaSources():
-    pass
 
 def VacationAndTravel():
     pass
